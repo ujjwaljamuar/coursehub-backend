a9a7e50f766e3ddec25c7c98767ef2267e3e7152
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-08 12:55:29 +0530

    added cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: `${process.env.FRONTEND_URL}`,
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
