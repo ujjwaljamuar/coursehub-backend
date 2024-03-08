760e933b563ded95403f6a5a2c203cb4e19d9917
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-08 15:40:00 +0530

    changed nodecron schedule

--- server.js
+++ server.js
@@ -8,24 +8,24 @@ import { Stats } from "./models/Stats.js";
 connectDB();
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-  api_key: process.env.CLOUDINARY_CLIENT_API,
-  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+    api_key: process.env.CLOUDINARY_CLIENT_API,
+    api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
 export const instance = new RazorPay({
-  key_id: process.env.RAZORPAY_API_KEY,
-  key_secret: process.env.RAZORPAY_API_SECRET,
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
-nodeCron.schedule("* * * * * *", async () => {
-  try {
-    await Stats.create({});
-  } catch (error) {
-    console.log(error);
-  }
+nodeCron.schedule("0 0 */1 * *", async () => {
+    try {
+        await Stats.create({});
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is working on port: ${process.env.PORT}`);
+    console.log(`Server is working on port: ${process.env.PORT}`);
 });
