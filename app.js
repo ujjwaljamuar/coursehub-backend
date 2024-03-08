19e2b816e2ec302fac324336ec433a01317480ab
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-08 11:56:43 +0530

    changed exports

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 config({
-  path: "./config/config.env",
+    path: "./config/config.env",
 });
 const app = express();
 
 // Using Middlewares
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    })
 );
 
 // Importing & Using Routes
@@ -36,12 +36,12 @@ app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
-export default app;
-
 app.get("/", (req, res) =>
-  res.send(
-    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
-  )
+    res.send(
+        `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+    )
 );
 
 app.use(ErrorMiddleware);
+
+export default app;
