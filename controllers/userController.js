bbfb34a28403657c5e39f786adaa406ebef7b822
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-09 17:32:35 +0530

    changed logout func

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,9 +57,9 @@ export const login = catchAsyncError(async (req, res, next) => {
 export const logout = catchAsyncError(async (req, res, next) => {
   res
     .status(200)
-    .cookie("token", "", {
+    .cookie("token", null, {
       expires: new Date(Date.now()),
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "none",
     })
