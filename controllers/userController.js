af7e424e42a4d033845932ff87e007e9129a0291
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-09 17:42:45 +0530

    changed logout func

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,16 +57,17 @@ export const login = catchAsyncError(async (req, res, next) => {
 export const logout = catchAsyncError(async (req, res, next) => {
   res
     .status(200)
-    .cookie("token", null, {
-      expires: new Date(Date.now()),
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    })
+    .clearCookie('token')
     .json({
       success: true,
       message: "Logged Out Successfully",
     });
+    // .cookie("token", null, {
+    //   expires: new Date(Date.now()),
+    //   httpOnly: true,
+    //   secure: true,
+    //   sameSite: "none",
+    // })
 });
 
 export const getMyProfile = catchAsyncError(async (req, res, next) => {
